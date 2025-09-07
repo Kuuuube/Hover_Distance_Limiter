8f551ce04f98bc17f8bc14cf554856979e78396d
kuuuube <hexagonisalie@gmail.com>
2025-09-07 13:16:48 -0400

    Remove out of date build script

--- build.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item -Recurse -Force bld
-$options = @('--configuration', 'Release', '-p:DebugType=embedded')
-dotnet publish ./Hover_Distance_Limiter $options --runtime win-x64 --framework net5 -o ./bld
-
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
